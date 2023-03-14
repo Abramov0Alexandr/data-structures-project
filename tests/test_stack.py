29da06ca5c2dd7b2b6e0a9a8c2e2ff7334a49814
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-14 17:01:21 +0300

    Дописал тесты для метода pop

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -10,6 +10,12 @@ class TestStack(unittest.TestCase):
         self.test_stack.push('test')
         self.assertEqual(self.test_stack, self.test_stack)
 
-    def test_pop_from_stack(self):
+    def test_get_none_with_pop(self):
+        self.test_stack.push('data1')
         self.test_stack.pop()
-        self.assertEqual(self.test_stack, self.test_stack)
+        self.assertIsNone(self.test_stack.top)
+
+    def test_get_el_with_pop(self):
+        self.test_stack.push('data1')
+        data = self.test_stack.pop()
+        self.assertEqual(data, 'data1')
