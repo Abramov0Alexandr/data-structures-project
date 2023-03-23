ed678863140e51fde9024eff67adebfca2108b95
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-23 15:51:01 +0300

    Написал тест для измененного метода удаления

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -12,16 +12,16 @@ class TestStack(unittest.TestCase):
         self.assertEqual(test_queue.head.data, 'data1')
         self.assertEqual(test_queue.head.next_node.data, 'data2')
 
-    def test_dequeue(self):
+    def test_empty_queue(self):
         test_queue = queue.Queue()
         test_queue.enqueue('data1')
+        test_queue.enqueue('data2')
+        self.assertEqual(test_queue.dequeue(), 'data1')
         test_queue.dequeue()
-        with self.assertRaises(AttributeError):
-            test_queue.head.data
+        self.assertIsNone(test_queue.dequeue())
 
     def test_str_for_queue(self):
         test_queue = queue.Queue()
         test_queue.enqueue('data1')
         test_queue.enqueue('data2')
         self.assertEqual(str(test_queue), "data1\ndata2")
-
