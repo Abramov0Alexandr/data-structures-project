9745195e27caab25b3f83694e66a78889acf9feb
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-28 19:03:15 +0300

    Написал заготовку под тесты

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    test_ll = LinkedList()
+
+
+
+
