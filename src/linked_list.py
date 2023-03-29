bdf6dbda43b6d6450bab19b4f26b37ef90992fc5
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-29 15:31:52 +0300

    Полностью переписал класс LinkedList и его методы

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,32 +1,42 @@
 class Node:
     """Класс для узла односвязного списка"""
-    def __init__(self, data: dict, next_node=None) -> None:
+    def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
-    def __init__(self):
-        self.head = None
+
+    test_list = []
+
+    def __init__(self, head=None, end=None):
+
+        self.head = head
+        self.end = end
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        new_node = Node(data, self.head)
-        self.head = new_node
+        beginning_node = Node(data)
+
+        if self.head is None:
+            self.end = beginning_node
+
+        else:
+            beginning_node.next_node = self.head
+
+        self.head = beginning_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        new_node = Node(data)
+
+        end_node = Node(data)
         if self.head is None:
-            self.head = new_node
-            return
+            self.head = end_node
 
-        last_node = self.head
-        while last_node.next_node:
-            last_node = last_node.next_node
-
-        last_node.next_node = new_node
+        else:
+            self.end.next_node = end_node
+        self.end = end_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
