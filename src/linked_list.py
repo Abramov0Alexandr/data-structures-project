adcf45bcb62a5574864fe8d464f397c68a542339
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-31 20:56:26 +0300

    Написал дополнительные методы для LinkedList

--- src/linked_list.py
+++ src/linked_list.py
@@ -8,33 +8,54 @@ class Node:
 class LinkedList:
     """Класс для односвязного списка"""
 
+    data_list: list = []
+
     def __init__(self, head=None, end=None):
 
         self.head = head
         self.end = end
 
-    def insert_beginning(self, data: dict) -> None:
+    def insert_beginning(self, data: dict):
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        beginning_node = Node(data)
+        if self.__verify_data(data):
+            beginning_node = Node(data)
 
-        if self.head is None:
-            self.end = beginning_node
+            if self.head is None:
+                self.end = beginning_node
 
-        else:
-            beginning_node.next_node = self.head
+            else:
+                beginning_node.next_node = self.head
 
-        self.head = beginning_node
+            self.head = beginning_node
 
-    def insert_at_end(self, data: dict) -> None:
+    def insert_at_end(self, data: dict):
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
 
-        end_node = Node(data)
-        if self.head is None:
-            self.head = end_node
+        if self.__verify_data(data):
+            end_node = Node(data)
+            if self.head is None:
+                self.head = end_node
 
-        else:
-            self.end.next_node = end_node
-        self.end = end_node
+            else:
+                self.end.next_node = end_node
+            self.end = end_node
+
+    def to_list(self):
+        """Метод добавляет все значения узлов в список data_list"""
+        node = self.head
+
+        while node.next_node is not None:
+            self.data_list.append(node.data)
+            node = node.next_node
+
+        self.data_list.append(node.data)
+        return self.data_list
+
+    def get_data_by_id(self, id_value) -> dict:
+        """Метод позволяет получить элемент по его id из списка data_list"""
+        for i in self.data_list:
+            if i['id'] == id_value:
+                return i
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -49,3 +70,10 @@ class LinkedList:
 
         ll_string += " None"
         return ll_string.strip()
+
+    @staticmethod
+    def __verify_data(data):
+        """Метод для проверки допустимости значений перед добавлением их в узел"""
+        if isinstance(data, dict) and data.get('id') is not None:
+            return True
+        print('Данные не являются словарем или в словаре нет ключа "id"')
